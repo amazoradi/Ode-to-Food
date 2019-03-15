eb1d9ba1e02b07944966fa6332e72f5fc8dfe028
Austin Zoradi <amazoradi@gmail.com>
2019-03-15 11:30:15 -0500

    module 4 complete

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        public IHtmlHelper HtmlHelper { get; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.HtmlHelper = htmlHelper;
+        }
+
+        public IActionResult OnGet(int? restaurantId)
+        {
+
+
+            Cuisines = HtmlHelper.GetEnumSelectList<CuisineType>();
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+            }
+
+
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Cuisines = HtmlHelper.GetEnumSelectList<CuisineType>();
+
+                return Page();
+            }
+            if (Restaurant.Id > 0)
+            {
+                restaurantData.Update(Restaurant);
+            }
+            else
+            {
+                restaurantData.Add(Restaurant);
+            }
+
+            restaurantData.Commit();
+            TempData["Message"] = "Restaurant Saved";
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+
+
+        }
+    }
+}
